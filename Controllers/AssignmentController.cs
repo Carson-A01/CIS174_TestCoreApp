e37a3648decd18a2d50824c10785fe4145d1669f
Carson-A01 <156151607+Carson-A01@users.noreply.github.com>
2024-02-17 18:20:24 -0600

    Add project files.

---
+++ Controllers/AssignmentController.cs
@@ -0,0 +1,48 @@
+﻿using CIS174_TestCoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIS174_TestCoreApp.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult List(int access)
+        {
+            ViewBag.Access = access;
+            List<Student> studentList = new List<Student>();
+
+            Student student = new Student();
+            student.firstName = "Gabe";
+            student.lastName = "Clark";
+            student.grade = "A";
+            studentList.Add(student);
+
+            student = new Student();
+            student.firstName = "Bethany";
+            student.lastName = "Michaels";
+            student.grade = "B";
+            studentList.Add(student);
+
+            student = new Student();
+            student.firstName = "Gabriela";
+            student.lastName = "Clark";
+            student.grade = "A+";
+            studentList.Add(student);
+
+            studentList.ToString();
+            /*if (access <= 2)
+            {
+                studentList = null;
+            }
+            else if((access > 2) && (access < 8))
+            {
+                studentList = studentList.Select(p => p.firstName).ToList();
+            }*/
+            return View(studentList);
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CIS174_TestCoreApp.Models
+{
+    public class Student
+    {
+        [Required]
+        public string firstName { get; set; } = String.Empty;
+        [Required]
+        public string lastName { get; set; } = String.Empty;
+        [Required]
+        public string grade { get; set; } = String.Empty;
+    }
+}
